3ec8c3c366b91455db2cd30419155a21b2277f9d
Georg Walther <georg.walther@rocket-internet.de>
2015-10-28 13:07:28 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (
+    print_function, division, generators,
+    absolute_import, unicode_literals
+)
+
+from setuptools import setup
+
+
+setup(
+    name='bonspy',
+    version='0.0.1',
+    description='Library that converts bidding trees to the AppNexus Bonsai language.',
+    authors=['Alexander Volkmann', 'Georg Walther'],
+    license='3-Clause BSD'
+)
