18c147eab434c670e794d8f01ff19c105f925a47
Georg Walther <georg.walther@rocket-internet.de>
2015-11-05 10:29:16 +0100

    update setup

--- setup.py
+++ setup.py
@@ -13,8 +13,14 @@ setup(
     version='0.0.1',
     description='Library that converts bidding trees to the AppNexus Bonsai language.',
     author='Alexander Volkmann, Georg Walther',
-    license='3-Clause BSD',
     packages=['bonspy'],
     package_dir={'bonspy': 'bonspy'},
-    url='https://github.com/mathemads/bonspy'
+    url='https://github.com/mathemads/bonspy',
+    download_url='https://github.com/mathemads/bonspy/tarball/master',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
